aa03663c037a6b365ff1263d325f7811d1ca0e2d
paulo.ornelas <pviniciusornelas@gmail.com>
2018-11-13 22:09:37 -0200

    Projeto Curso Angular

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipes.model';
+
+@Component({
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('Bolo', 'Bolo de brigadeiro',
+         'https://guiadacozinha.com.br/wp-content/uploads/2017/03/bolo-brigadeiro-confeitado.jpg')
+    ];
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
